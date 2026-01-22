682b43d890a11611af32f7be615eef19d2846f6c
helium-service <helium-service@chartboost.com>
2026-01-22 11:47:20 -0800

    [AUTO-GENERATED] Synchronizing changes from private repository

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ In order to add the Chartboost Mediation Unity SDK - Pangle Adapter to your proj
 
 ```json
 "dependencies": {
-    "com.chartboost.mediation.unity.adapter.pangle": "5.1.2",
+    "com.chartboost.mediation.unity.adapter.pangle": "5.1.3",
     ...
 },
 "scopedRegistries": [
--- Runtime/Pangle/PangleAdapter.cs
+++ Runtime/Pangle/PangleAdapter.cs
@@ -13,7 +13,7 @@ namespace Chartboost.Mediation.Pangle
         /// <summary>
         /// The partner adapter Unity version.
         /// </summary>
-        public const string AdapterUnityVersion = "5.1.2";
+        public const string AdapterUnityVersion = "5.1.3";
         
         /// <inheritdoc cref="IPartnerAdapterConfiguration.AdapterNativeVersion"/>
         public static string AdapterNativeVersion => Instance.AdapterNativeVersion;
